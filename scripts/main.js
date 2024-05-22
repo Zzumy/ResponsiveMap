df7e2641dc61fbd419415d58fb4e610aa7340ff6
Dark2Knight7 <ducpeti27@gmail.com>
2024-05-22 18:18:08 +0200

    friss

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,206 @@
 $(document).ready(function () {
+    let slideIndex = {};
+    $(".slider").each(function () {
+        let sliderId = $(this).attr("id");
+        slideIndex[sliderId] = 1;
+        showSlides(sliderId, slideIndex[sliderId]);
+    });
+    function plusSlides(sliderId, n) {
+        showSlides(sliderId, (slideIndex[sliderId] += n));
+    }
+    function currentSlide(sliderId, n) {
+        showSlides(sliderId, (slideIndex[sliderId] = n));
+    }
+    function showSlides(sliderId, n) {
+        let i;
+        let slides = $(`#${sliderId} .mySlides`);
+        let dots = $(`#${sliderId} .dot`);
+        if (n > slides.length) {
+            slideIndex[sliderId] = 1;
+        }
+        if (n < 1) {
+            slideIndex[sliderId] = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            $(slides[i]).css("display", "none");
+        }
+        for (i = 0; i < dots.length; i++) {
+            $(dots[i]).removeClass("active");
+        }
+        $(slides[slideIndex[sliderId] - 1]).css("display", "block");
+        $(dots[slideIndex[sliderId] - 1]).addClass("active");
+    }
+    $(".prev").on("click", function () {
+        let sliderId = $(this).closest(".slider").attr("id");
+        plusSlides(sliderId, -1);
+    });
+    $(".next").on("click", function () {
+        let sliderId = $(this).closest(".slider").attr("id");
+        plusSlides(sliderId, 1);
+    });
+    $(".dot").on("click", function () {
+        let sliderId = $(this).closest(".slider").attr("id");
+        currentSlide(sliderId, $(this).index() + 1);
+    });
     let x = 0;
-    let y = 94.4;
+    let y = 148;
+    let zoom = 1;
     let img = $("#map");
     let isDragging = false;
-
-    // Function to handle panel toggle
     function togglePanel() {
         let panel = $("#panel");
         let toggleButton = $("#togglePanelButton");
         let toggleButtonIcon = $("#panelToggleButtonIcon");
 
         if (panel.css("transform") === "matrix(1, 0, 0, 1, 0, 0)") {
-            panel.css("transform", "translateY(-350px)"); // Módosítás: Felfelé elrejtés
-            panel.css("top", "90"); // Módosítás: tetejére helyezés
-            toggleButtonIcon.attr("class", "bi bi-chevron-down"); // Change icon to right arrow
+            panel.css("transform", "translateY(-555px)");
+            panel.css("top", "90");
+            toggleButtonIcon.attr("class", "bi bi-chevron-down");
         } else {
-            panel.css("transform", "translateY(0)"); // Módosítás: Felfelé megjelenítés
-            panel.css("top", null); // Módosítás: alaphelyzet visszaállítása
-            toggleButton.css("transform", "translateY(0)"); // Módosítás: gomb megjelenítése
-            toggleButtonIcon.attr("class", "bi bi-chevron-up"); // Change icon to left arrow
+            panel.css("transform", "translateY(0)");
+            panel.css("top", null);
+            toggleButton.css("transform", "translateY(0)");
+            toggleButtonIcon.attr("class", "bi bi-chevron-up");
         }
     }
-
-    // Mouse down event for map dragging
     img.on("mousedown", function (event) {
         isDragging = true;
         let offset = img.offset();
-        x = event.clientX - offset.left;
-        y = event.clientY - offset.top;
+        x =
+            event.clientX -
+            offset.left +
+            (img.width() - img.width() * zoom) / 2;
+        y =
+            event.clientY -
+            offset.top +
+            (img.height() - img.height() * zoom) / 2;
     });
-
-    // Mouse up event for ending dragging
     $(document).on("mouseup", function (event) {
         if (isDragging) {
             isDragging = false;
         }
     });
-
-    // Mouse move event for dragging
     $(document).on("mousemove", function (event) {
         if (isDragging) {
             let newX = event.clientX - x;
             let newY = event.clientY - y;
-
-            // Limiting movement within bounds of map container
             let parentWidth = img.parent().width();
             let parentHeight = img.parent().height();
             let imgWidth = img.width();
             let imgHeight = img.height();
-
-            if (newX > 0) {
-                newX = 0;
-            } else if (newX < parentWidth - imgWidth) {
-                newX = parentWidth - imgWidth;
+            if (newX > 0 - (img.width() - img.width() * zoom) / 2) {
+                newX = 0 - (img.width() - img.width() * zoom) / 2;
+            } else if (
+                newX <
+                parentWidth - imgWidth + (img.width() - img.width() * zoom) / 2
+            ) {
+                newX =
+                    parentWidth -
+                    imgWidth +
+                    (img.width() - img.width() * zoom) / 2;
             }
-            if (newY > 94.4) {
-                newY = 94.4;
-            } else if (newY < parentHeight - imgHeight + innerHeight) {
-                newY = parentHeight - imgHeight + innerHeight;
+            if (newY > 148 - (img.height() - img.height() * zoom) / 2) {
+                newY = 148 - (img.height() - img.height() * zoom) / 2;
+            } else if (
+                newY <
+                parentHeight -
+                    imgHeight +
+                    innerHeight +
+                    (img.height() - img.height() * zoom) / 2
+            ) {
+                newY =
+                    parentHeight -
+                    imgHeight +
+                    innerHeight +
+                    (img.height() - img.height() * zoom) / 2;
             }
-
             img.css({left: newX + "px", top: newY + "px"});
         }
-
-        // Preventing navigation when dragging near bottom edge of map
         if (event.clientY > window.innerHeight - 50) {
             event.preventDefault();
         }
     });
-
-    // Hamburger menu click event
+    img.on("wheel", function (e) {
+        e.preventDefault();
+        if (e.originalEvent.deltaY < 0) {
+            zoom += 0.1;
+        } else {
+            zoom -= 0.1;
+        }
+        zoom = Math.min(Math.max(1, zoom), 3);
+        let newX = e.clientX - x;
+        let newY = e.clientY - y;
+        let parentWidth = img.parent().width();
+        let parentHeight = img.parent().height();
+        let imgWidth = img.width();
+        let imgHeight = img.height();
+        if (newX > 0 - (img.width() - img.width() * zoom) / 2) {
+            newX = 0 - (img.width() - img.width() * zoom) / 2;
+        } else if (
+            newX <
+            parentWidth - imgWidth + (img.width() - img.width() * zoom) / 2
+        ) {
+            newX =
+                parentWidth - imgWidth + (img.width() - img.width() * zoom) / 2;
+        }
+        if (newY > 148 - (img.height() - img.height() * zoom) / 2) {
+            newY = 148 - (img.height() - img.height() * zoom) / 2;
+        } else if (
+            newY <
+            parentHeight -
+                imgHeight +
+                innerHeight +
+                (img.height() - img.height() * zoom) / 2
+        ) {
+            newY =
+                parentHeight -
+                imgHeight +
+                innerHeight +
+                (img.height() - img.height() * zoom) / 2;
+        }
+        img.css({left: newX + "px", top: newY + "px"});
+        img.css("transform", `scale(${zoom})`);
+    });
     $("#hamburger").on("click", function () {
         let x = $("#myTopnav");
-        let y = $("#panel")
+        let y = $("#panel");
         x.toggleClass("responsive");
         y.toggleClass("responsive");
     });
-
-    // Panel toggle button click event
     $("#togglePanelButton").on("click", function () {
         togglePanel();
     });
-
-    // Adjust toggle button position initially
+    $("#cent button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expCent").show();
+    });
+    $("#wats button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expWats").show();
+    });
+    $("#hey button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expHey").show();
+    });
+    $("#west button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expWest").show();
+    });
+    $("#santo button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expSanto").show();
+    });
+    $("#pacific button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expPacific").show();
+    });
+    $("#out button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expOut").show();
+    });
+    $("#dog button").on("click", function () {
+        $("#summary > div").hide();
+        $("#expDog").show();
+    });
     togglePanel();
 });
